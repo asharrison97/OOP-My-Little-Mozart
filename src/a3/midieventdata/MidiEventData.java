d5befd264b13f23a569fe83e1e3ec895780fc540
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-04-02 16:23:27 -0500

    fixed tabbing in midieventdata

--- src/a3/midieventdata/MidiEventData.java
+++ src/a3/midieventdata/MidiEventData.java
@@ -3,14 +3,13 @@ package a3.midieventdata;
 public class MidiEventData {
 	private int startEndTick, velocity, note, channel, noteOnOff;
 	private int instrument;
-	public MidiEventData (int startEndTick, int velocity, int note,
-	int channel, int instrument, int noteOnOff) {
-	this.startEndTick = startEndTick ;
-	this.velocity = velocity;
-	this.note = note ;
-	this.channel = channel;
-	this.instrument = instrument;
-	this.noteOnOff = noteOnOff ;
+	public MidiEventData (int startEndTick, int velocity, int note, int channel, int instrument, int noteOnOff) {
+		this.startEndTick = startEndTick ;
+		this.velocity = velocity;
+		this.note = note ;
+		this.channel = channel;
+		this.instrument = instrument;
+		this.noteOnOff = noteOnOff ;
 	}
 	
 	public int getStartEndTick() {
