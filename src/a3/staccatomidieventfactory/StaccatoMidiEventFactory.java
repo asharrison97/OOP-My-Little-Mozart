a540a6814f5034ddbaeefac1edca3103fa3eb892
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-02 16:49:52 -0500

    Added different midi event factory packages/classes for the different types we will be using in this project. Created an interface for these classes to implement

---
+++ src/a3/staccatomidieventfactory/StaccatoMidiEventFactory.java
@@ -0,0 +1,22 @@
+package a3.staccatomidieventfactory;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiEvent;
+
+import a3.midieventfactory.MidiEventFactory;
+
+public class StaccatoMidiEventFactory implements MidiEventFactory {
+
+	@Override
+	public MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
