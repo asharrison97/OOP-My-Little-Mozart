90c7213d86b772e172a3251e4c4748deb0436b1d
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-04-03 10:07:34 -0500

    started working on the csv parser

--- src/a3/midicsvparser/MidiCsvParser.java
+++ src/a3/midicsvparser/MidiCsvParser.java
@@ -1,5 +1,14 @@
 package a3.midicsvparser;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.*;
+import a3.midieventdata.*;
+
 
 public class MidiCsvParser {
-
+	public static List<MidiEventData> parseCsv(File f) {
+		List<MidiEventData> midiDataList = new ArrayList<MidiEventData>();
+		
+		return midiDataList;
+	}
 }
