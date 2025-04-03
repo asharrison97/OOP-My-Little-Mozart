aaed8cb5399ca8c19f2f8f09718b53aa19de13de
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-04-03 13:55:10 -0500

    added midi factory abstract

--- src/a3/midieventfactory/MidiEventFactory.java
+++ src/a3/midieventfactory/MidiEventFactory.java
@@ -6,11 +6,3 @@ public interface MidiEventFactory {
 	MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
 	MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 }
-
-
-
-
-
-
-
-
---
+++ src/a3/midieventfactoryabstract/MidiEventFactoryAbstract.java
@@ -0,0 +1,7 @@
+package a3.midieventfactoryabstract;
+
+import a3.midieventfactory.*;
+
+public interface MidiEventFactoryAbstract {
+		MidiEventFactory createFactory();
+}
