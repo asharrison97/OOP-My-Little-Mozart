6f2df29ef74162e3a0e4e82a373e01ffde070322
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-04-04 12:15:16 -0500

    added trumpet strategy

---
+++ src/a3/instrumentstrategy/TrumpetStrategy.java
@@ -0,0 +1,9 @@
+package a3.instrumentstrategy;
+
+import javax.sound.midi.Track;
+
+public class TrumpetStrategy implements InstrumentStrategy{
+	public void applyInstrument(Track track, int channel) {
+		
+	}
+}
