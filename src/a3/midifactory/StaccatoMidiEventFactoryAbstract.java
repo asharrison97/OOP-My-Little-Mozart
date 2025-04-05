c42a2e65186d09e781413d9a9b48f8f2b8c3e8b8
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 17:01:19 -0500

    Added code to StaccatoMidiEventFactoryAbstract.java and javadoc.

--- src/a3/midifactory/StaccatoMidiEventFactoryAbstract.java
+++ src/a3/midifactory/StaccatoMidiEventFactoryAbstract.java
@@ -1,5 +1,14 @@
 package a3.midifactory;
 
-public class StaccatoMidiEventFactoryAbstract {
-
+/**
+ * public class StaccatoMidiEventFactoryAbstract - Implements MidiEventFactoryAbstract
+ * interface, designed to create and return an empty StaccatoMidiEvent factory.
+ */
+public class StaccatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract{
+	public MidiEventFactory createFactory() {
+		MidiEventFactory newFactory = new StaccatoMidiEventFactory();
+		
+		return newFactory;
+	}
 }
+
