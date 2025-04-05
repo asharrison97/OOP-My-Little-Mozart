61a1c9777e30403c723e8b4884c2868aaee05f33
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 16:05:20 -0500

    Added javadoc to MidiEventFactory.java file.

--- src/a3/midifactory/MidiEventFactory.java
+++ src/a3/midifactory/MidiEventFactory.java
@@ -2,7 +2,19 @@ package a3.midifactory;
 
 import javax.sound.midi.*;
 
+/**
+ * MidiEventFactory interface, which will be implemented by our midi event factory class.
+ */
 public interface MidiEventFactory {
+	/**
+	 * Creates an on/off note
+	 * @param tick - which tick is the note on
+	 * @param note - note data
+	 * @param velocity - note velocity
+	 * @param channel - note channel, to assign instruments
+	 * @return
+	 * @throws InvalidMidiDataException - if invalid data is passed
+	 */
 	MidiEvent createNoteOn(int tick, int note, int velocity, int channel) throws InvalidMidiDataException;
 	MidiEvent createNoteOff(int tick, int note, int channel) throws InvalidMidiDataException;
 }
