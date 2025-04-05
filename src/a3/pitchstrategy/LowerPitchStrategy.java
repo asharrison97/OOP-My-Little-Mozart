7156f1aa8183a09373a30cd8e5fc99d5fa2d36a2
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 16:19:02 -0500

    Added javadoc code to LowerPitchStrategy.java

--- src/a3/pitchstrategy/LowerPitchStrategy.java
+++ src/a3/pitchstrategy/LowerPitchStrategy.java
@@ -1,6 +1,16 @@
 package a3.pitchstrategy;
 
+/**
+ * public class LowerPitchStrategy - Implements the pitch strategy 
+ * interface, this class contains a method designed to lower the pitch
+ * of a note
+ */
 public class LowerPitchStrategy implements PitchStrategy{
+	/**
+	 * public int modifyPitch() - This method will return the same note
+	 * that it was passed, however the pitch will be slightly lower.
+	 * @param note - The note which will be returned with a lower pitch.
+	 */
 	public int modifyPitch(int note) {
 		int transposedNote = note - 2;
 		
