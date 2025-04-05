fb2eed7fa50a776173b5cd17f3940d518d32c06e
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 16:07:30 -0500

    Added javadoc to MidiEventFactoryAbstract.java

--- src/a3/midifactory/MidiEventFactoryAbstract.java
+++ src/a3/midifactory/MidiEventFactoryAbstract.java
@@ -1,5 +1,12 @@
 package a3.midifactory;
 
+/**
+ * MidiEventFactoryAbstract interface, which will be implemented by our different midi event factory classes, which are standard, staccato, and legato.
+ */
 public interface MidiEventFactoryAbstract {
+	/**
+	 * Method will be implemented later, with the goal of generating factories based on whichever type we need.
+	 * @return
+	 */
 	MidiEventFactory createFactory();
 }
