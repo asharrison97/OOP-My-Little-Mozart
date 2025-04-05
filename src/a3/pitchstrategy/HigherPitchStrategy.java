96c2c230ff424685468eb562c31a582c39edb432
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 16:17:18 -0500

    Added javadoc to HigherPitchStrategy.java

--- src/a3/pitchstrategy/HigherPitchStrategy.java
+++ src/a3/pitchstrategy/HigherPitchStrategy.java
@@ -1,6 +1,16 @@
 package a3.pitchstrategy;
 
+/**
+ * public class HigherPitchStrategy() - This class implements the PitchStrategy interface and contains a method
+ * designed to modify the pitch and make it higher.
+ */
 public class HigherPitchStrategy implements PitchStrategy{
+	/**
+	 * public int modifyPitch() - This method will return a note
+	 * that contains the same data as the note passed to the method,
+	 * however the pitch will be higher.
+	 * @param note - Note data which will be modified.
+	 */
 	public int modifyPitch(int note) {
 		int transposedNote = note + 2;
 		
