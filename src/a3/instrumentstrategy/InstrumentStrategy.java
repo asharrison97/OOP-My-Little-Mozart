30f39d9bac7f7f71fd3f1ef90140cb9d93fc2773
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 16:36:53 -0500

    Added javadoc to InstrumentStrategy.java

--- src/a3/instrumentstrategy/InstrumentStrategy.java
+++ src/a3/instrumentstrategy/InstrumentStrategy.java
@@ -1,6 +1,10 @@
 package a3.instrumentstrategy;
 import javax.sound.midi.*;
 
+/**
+ * public interface InstrumentStrategy - This interface will be implemented in
+ * future classes that will be designed to modify the instrument within a track.
+ */
 public interface InstrumentStrategy {
 	void applyInstrument(Track track, int channel);
 }
