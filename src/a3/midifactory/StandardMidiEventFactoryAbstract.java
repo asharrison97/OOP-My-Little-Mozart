b8ab46b619395cfbd6a0bb8c0bb375790ceefc2a
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 16:14:31 -0500

    Added javadoc to StandardMidiEventFactoryAbstract.java

--- src/a3/midifactory/StandardMidiEventFactoryAbstract.java
+++ src/a3/midifactory/StandardMidiEventFactoryAbstract.java
@@ -1,6 +1,14 @@
 package a3.midifactory;
 
+/**
+ * public class StandardMidiEventFactoryAbstract - Implements MidiEventFactoryAbstract interface,
+ * Contains a method that creates and returns a factory object of MidiEventFactory type.
+ */
 public class StandardMidiEventFactoryAbstract implements MidiEventFactoryAbstract{
+	/**
+	 * public MidiEventFactory createFactory()
+	 * This method will return an empty factory object of type MidiEventFactory.
+	 */
 	public MidiEventFactory createFactory() {
 		MidiEventFactory newFactory = new StandardMidiEventFactory();
 		
