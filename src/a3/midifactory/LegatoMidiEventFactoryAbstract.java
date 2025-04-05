dff9fd7a482a64fe42d2832bd15b67bd9b312592
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 17:29:43 -0500

    Added code to LegatoMidiEventFactoryAbstract.java

--- src/a3/midifactory/LegatoMidiEventFactoryAbstract.java
+++ src/a3/midifactory/LegatoMidiEventFactoryAbstract.java
@@ -1,5 +1,13 @@
 package a3.midifactory;
 
-public class LegatoMidiEventFactoryAbstract {
-
+/**
+ * public class LegatoMidiEventFactoryAbstract - Implements MidiEventFactoryAbstract
+ * interface, designed to create and return an empty StaccatoMidiEvent factory.
+ */
+public class LegatoMidiEventFactoryAbstract implements MidiEventFactoryAbstract{
+	public MidiEventFactory createFactory() {
+		MidiEventFactory newFactory = new LegatoMidiEventFactory();
+		
+		return newFactory;
+	}
 }
