7e531a2a9a87a17c915c57fb1aa884e47c29b5d7
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-04-05 16:19:51 -0500

    Added javadoc code to PitchStrategy.java

--- src/a3/pitchstrategy/PitchStrategy.java
+++ src/a3/pitchstrategy/PitchStrategy.java
@@ -1,5 +1,10 @@
 package a3.pitchstrategy;
 
+/** 
+ * public interface PitchStrategy - This interface will be 
+ * implemented in future classes that will be used to lower the 
+ * pitch of notes.
+ */
 public interface PitchStrategy {
 	int modifyPitch(int note);
 }
